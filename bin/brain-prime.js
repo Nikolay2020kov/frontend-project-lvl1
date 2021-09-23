58b554b2512a749bae014dd31502aa844dcd4976
Nikolay2020kov <nkovalev631@gmail.com>
2021-09-23 22:01:08 +0300

    yrok 9

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,17 +1,32 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
+const fun = (n) => {
+  if (n === 1) {
+    return false;
+  }
+  if (n === 2) {
+    return true;
+  }
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const func = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`'Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
   const getRandomNumber = () => Math.floor(Math.random() * 100);
   for (let i = 1; i <= 3; i += 1) {
     const number = getRandomNumber();
     console.log(`Question: ${number}`);
     const youAnswer = readlineSync.question('You answer: ');
-    if (number % 2 === 0) {
+    if (fun(number) === true) {
       if (youAnswer === 'yes') {
         console.log('Correct!');
       } else {
@@ -20,7 +35,7 @@ const func = () => {
         break;
       }
     }
-    if (number % 2 !== 0) {
+    if (fun(number) === false) {
       if (youAnswer === 'no') {
         console.log('Correct!');
       } else {
